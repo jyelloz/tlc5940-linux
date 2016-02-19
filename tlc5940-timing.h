c5fcd44bfbbced48e62983831b1bd1935e62583c
Jordan Yelloz <jordan@yelloz.me>
2016-02-18 22:24:17 -0800

    removed unused header file

--- tlc5940-timing.h
+++
@@ -1,8 +0,0 @@
-#ifndef TLC5940_TIMING_H
-#define TLC5940_TIMING_H
-
-#define GSCLK_SPEED_HZ  2500000
-#define GSCLK_PERIOD_NS ((unsigned long) (1e9 / GSCLK_SPEED_HZ))
-#define BLANK_PERIOD_NS (4096 * GSCLK_PERIOD_NS)
-
-#endif /* TLC5940_TIMING_H */
